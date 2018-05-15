0a5c1e8ed3e53124d6c80b97c681627574c2ec4c
Jess Jacobson <jessanova@gmail.com>
2018-05-15 19:25:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object,key,value){
   return obj
 }
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
-  return Object.assign(object,{key:value})
+  return object[key]=value
 }
 function deleteFromObjectByKey(object,key){
   var obj = {}
